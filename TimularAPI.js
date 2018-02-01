9b8e62e0c49fde5f57ba6f91ffdbd42d44fae4d2
Christoph Bussenius <christoph.bussenius@sevenval.com>
2018-02-01 15:33:13 +0100

    Changes to TimularAPI
    reads list of times from Timular API, stores them, merges them, sets
    listEntry numbers and saves them to projectile

--- TimularAPI.js
+++ TimularAPI.js
@@ -1,56 +1,76 @@
-const request = require('request'); 
-const csv = require('csvtojson'); 
-const fs = require('fs');
-/* 
-var options = { method: 'GET',
-  url: 'https://api.timeular.com/api/v2/report/2018-01-01T00%3A00%3A00.000/2018-01-31T00%3A00%3A00.000',
-  qs: { timezone: 'Europe/Berlin' },
-  headers: 
-   { 'postman-token': 'b974cbfc-609b-703b-f11c-1ed107b8714c',
-     'cache-control': 'no-cache',
-     authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJ0eXBlIjoidXNlciIsInN1YiI6IjEzMDg2In0.j5KdbbpkkkLZlKQEk15vVop_HzuI2S7CNibqJ1JmGErJZHR6l4RG51yXY2UqVMbcRXvoN14_R-szY5ff-okn8w' } };
+const request = require('request');
+const index = require('./index.js');
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+// TO DO:  API TOKEN CONFIG
 
-  console.log(body);
-});
- */
-// TO DO:  API TOKEN CONFIG 
-
+let listentry = 0;
 let month = [];
+let monthCleaned = [];
 
-function getCSVFile(){ 
-    return new Promise((resolve,reject)=>{ 
-        request.get('https://api.timeular.com/api/v2/report/2018-01-01T00%3A00%3A00.000/2018-02-10T00%3A00%3A00.000',{ 
-            qs: { timezone:'Europe/Berlin', 
-           }, 
-           headers: { 
-               authorization:'Bearer eyJhbGciOiJIUzUxMiJ9.eyJ0eXBlIjoidXNlciIsInN1YiI6IjEzMDg2In0.j5KdbbpkkkLZlKQEk15vVop_HzuI2S7CNibqJ1JmGErJZHR6l4RG51yXY2UqVMbcRXvoN14_R-szY5ff-okn8w"eyJhbGciOiJIUzUxMiJ9.eyJ0eXBlIjoidXNlciIsInN1YiI6IjEzMDg2In0.j5KdbbpkkkLZlKQEk15vVop_HzuI2S7CNibqJ1JmGErJZHR6l4RG51yXY2UqVMbcRXvoN14_R-szY5ff-okn8w'
+function getTimeList(){
+    return new Promise((resolve,reject)=>{
+
+         // request.get('https://api.timeular.com/api/v2/time-entries/2017-01-01T00:00:00.000/2018-01-31T00:00:00.000',{
+         request.get('https://api.timeular.com/api/v2/time-entries/2018-02-01T00:00:00.000/2018-02-01T23:59:59.000',{
+           headers: {
+               Authorization:'Bearer eyJhbGciOiJIUzUxMiJ9.eyJ0eXBlIjoidXNlciIsInN1YiI6IjEzMzY3In0.uH9KRRnwKJlWtDo9BesixQG5UdIp-9TSzOUndPVbBuV0MyfXZwS0MN79AwKGqX4hX_sBBWoItVmaOIjVVXYRew',
+               Accept: 'application/json;charset=UTF-8'
            }
-        }, (err, res)=> { 
-             
-            csv({noheader:false}).fromString(res.body).on('csv', (csvRow)=> { 
-                let day = {};
-                day["StartDate"] = csvRow[2];
-                day["listEntry"] = 0;
-                day["Duration"] = csvRow[8]/60;
-                // extract Activity from activity string of form "text [jobnumber]"
-                day["Activity"] = csvRow[10].substring(csvRow[10].lastIndexOf("[")+1,csvRow[10].lastIndexOf("]"));
-                day["Note"] = csvRow[12];
-                month.push(day);
-                resolve(month);
-            }).on('done', ()=> { })
+        }, (err, res)=> {
+          let timeList = JSON.parse(res.body);
+
+          for (let i = 0; i < timeList.timeEntries.length; i++){
+            let day = {};
+            day["StartDate"] = timeList.timeEntries[i].duration.startedAt.substring(0, timeList.timeEntries[i].duration.startedAt.indexOf("T"));
+            day["listEntry"] = 0;
+            /*
+            timestamp is not accurate. sometimes 2h = 120min has a value thats != 120min. To solve that I cut the
+            seconds and milliseconds from the timestamp. Decreases precision --> alternative would be to round values
+            */
+            day["Duration"] = (Date.parse(timeList.timeEntries[i].duration.stoppedAt.substring(0, timeList.timeEntries[i].duration.stoppedAt.lastIndexOf(":"))) - Date.parse(timeList.timeEntries[i].duration.startedAt.substring(0, timeList.timeEntries[i].duration.startedAt.lastIndexOf(":"))))/60000;
+            day["Activity"] = timeList.timeEntries[i].activity.name.substring(timeList.timeEntries[i].activity.name.lastIndexOf("[")+1,timeList.timeEntries[i].activity.name.lastIndexOf("]"));
+            day["Note"] = timeList.timeEntries[i].note.text;
+            // console.log(day);
+            month.push(day);
+          }
+
+          // merging Duration time of dup Note lines for entries from same day
+          for (var i = 0; i<month.length; i++){
+            for (var j = i + 1; j<month.length; j++){ // j = i + 1 becuase .csv is sorted!
+              if((month[i]["StartDate"] === month[j]["StartDate"]) && (month[i]["Note"] === month[j]["Note"])){
+                month[i]["Duration"] = (month[i]["Duration"] * 60 + month[j]["Duration"] * 60 ) / 60;
+                month.splice(j, 1); // remove merged entry from original array, to avoid recounting them in next i increment
+                j--; // as one entry is spliced, the next candidate has the same j index number!
+              } else if (month[i]["StartDate"] !== month[j]["StartDate"]) {
+                break; // Date matches no longer? csv is sorted, break the comparison loop
+              }
+            }
+            monthCleaned.push(month[i]); // output the merged day entry to clean array
+          }
+
+          // set listEntry numbers for clean pushing to projectile
+          for (var i = 0; i<monthCleaned.length; i++){
+              if (i == monthCleaned.length-1){
+                  month[i]["listEntry"]=listentry;
+              } else{
+                  month[i]["listEntry"]=listentry;
+                  if(month[i]["StartDate"] === month[i+1]["StartDate"]){  // simpler
+                      listentry= listentry +1;
+                  } else {
+                      listentry = 0;
+                  }
+              }
+          }
+
+          monthCleaned.forEach((obj)=>{
+               index.save(obj["StartDate"], obj["listEntry"], obj["Duration"],  obj["Activity"], obj["Note"]);
+              // for testing only
+              // console.log(obj["StartDate"], obj["listEntry"], obj["Duration"],  obj["Activity"], obj["Note"]);
+          })
+
+
         })
     })
 }
 
-async function don(){ 
-    let a = await getCSVFile(); 
-    console.log(a);
-}
-
-
-don();
-/* 
-csv().fromStream(request.get('https:')) */
+getTimeList();
