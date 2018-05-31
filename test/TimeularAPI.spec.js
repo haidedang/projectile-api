1627aded91e4a17b7a15a637e26cd4b77e1ec024
Hai Dang <haiduc.dang91@gmail.com>
2018-05-31 14:18:48 +0200

    adjusting test to config parameters

--- test/TimeularAPI.spec.js
+++ test/TimeularAPI.spec.js
@@ -10,21 +10,38 @@ const host = "http://localhost:3001";
 
 chai.use(chaiHttp);
 
+// IMPORTANT NOTE FOR TESTING:
+// Set the parameter values in config.json before running the tests! 
+
+let config = {}; 
+let activityID = null; 
+
+// Initializing parameters from config and set token 
 before(function () {
+  config = JSON.parse(fs.readFileSync('config.json'));
+  activityID = config.test.timeular.TimularActivityID;
   let token = JSON.parse(fs.readFileSync('timeularToken.txt'));
   timeularAPI.initializeToken(token);
 })
 
-describe('get Activites from Timeular', function(){
-  it('should return all Activities', async ()=> {
-    let result = await timeularAPI.getActivities(); 
-    expect(result.activities.length).to.not.equal(0); 
+describe('get Activites/Activity from Timeular', function () {
+  it('should return all Activities', async () => {
+    let result = await timeularAPI.getActivities();
+    expect(result.activities.length).to.not.equal(0);
+  })
+
+  it('return specific Activity with package', async () => {
+    let result = await timeularAPI.packageActivityList(activityID); 
+    expect(result.Activity.includes(activityID)).to.be.true;
   })
 })
 
+
 describe('book an Entry in Timeular API', function () {
   it('should save successfully', async () => {
-    let response = await timeularAPI.bookActivityNG('2018-05-31', '1', '106114', 'hallo');
-    expect(response, 'returnValue of Save fn should be true').to.be.true;
+    let response = await timeularAPI.bookActivityNG('2018-05-31', '1', activityID, 'hallo');
+    expect(response, 'returnValue of Save function should be true').to.be.true;
   })
 })
+
+
--- test/projectileAPI.spec.js
+++ test/projectileAPI.spec.js
@@ -10,12 +10,22 @@ const host = "http://localhost:3001";
 
 chai.use(chaiHttp);
 
+before(function () {
+    config = JSON.parse(fs.readFileSync('config.json'));
+    activity = config.test.projectile.activity;
+    date = config.test.projectile.date; 
+    duration = config.test.projectile.duration; 
+    note = config.test.projectile.note; 
+    let user = JSON.parse(fs.readFileSync('user.txt'));
+    projectileAPI.initializeUser(user);
+  })
+
 
 describe('book an Entry in Projectile API', function() { 
     this.timeout(7000);
    
     it('it should save successfully', async () => { 
-      let response = await projectileAPI.save('2018-05-31',1,'2759-327','hallo'); 
+      let response = await projectileAPI.save(date,duration,activity,note); 
       expect(response.returnValue, 'returnValue of Save fn should be true').to.be.true; 
     })
   })
