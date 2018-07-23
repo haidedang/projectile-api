6bbfd9a91d3c224385f1f101d153cc045c6f10bd
Hai Dang <haiduc.dang91@gmail.com>
2018-07-23 16:04:23 +0200

    adding booking router

---
+++ Api/routes.booking.js
@@ -0,0 +1,9 @@
+const BookController = require('./controller.booking');
+
+module.exports = (app) => {
+  app.get('/book/:date?/:duration/:activity/:note',
+    BookController.bookEntry
+  );
+}
+
+
