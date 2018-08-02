1446be6fcc5ecc5338e028bf7c76df06885048ff
Hai Dang <haiduc.dang91@gmail.com>
2018-08-02 18:49:47 +0200

    add bookEntry

--- src/api/controllers/BookingController.js
+++ src/api/controllers/BookingController.js
@@ -1,3 +1,8 @@
+const logger = require('../../lib/logger');
+
+const projectile = require('../../../projectileAPI.js');
+const authenticationMiddleware = require('../../lib/AuthenticationMiddleware');
+
 class BookingController {
   /**
    * Static middleware to book an activity.
@@ -12,9 +17,68 @@ class BookingController {
    * @returns {void}
    */
   static async bookEntry(req, res) {
-    res.json({
-      status: 'ok'
-    });
+    /*  res.json({
+       status: 'ok'
+     });
+  */
+
+
+    console.log('reached Controller');
+    try {
+      /*   e.g.
+           http://localhost:3000/book/1/2788-3/testing
+           http://localhost:3000/book/2018-05-23/1.5/2788-3/testing
+          */
+      // books in timeular and projectile!
+      // TODO: check validity of date, duration, activitiy and note?
+
+      // check if date parameter is present or use current date
+      let date = '';
+      if (!req.body.date) {
+        date = new Date().toISOString().substr(0, 10); // YYYY/MM/DD
+      } else {
+        date = req.body.date;
+      }
+
+      // normalizing duration time if necessary (to x.xx and parse as float to avoid weird duration lengths)
+      let time = await projectile.normalizetime(req.body.duration);
+      time = parseFloat(time);
+      // book in projectile
+      /*
+           use activity directly when projectileOnly mode is active, else use Package value processed from timeular,
+           it allows to use activityId or packageId to be provided in url
+           */
+      const payload = await authenticationMiddleware.getPayload(req.token);
+      console.log(payload);
+      const cookie = payload.cookie;
+
+      projectile
+        .save(date, time, req.body.activity, req.body.note, cookie)
+        .then(result => {
+          logger.debug('save for projectile successfull');
+          // handle result of save request!! TODO
+          // res.status(200).send(date + ' ' + req.body.duration + ' ' + req.body.activity + ' ' + req.body.note);
+          if (result.resultValue == false) {
+            res.status(200).json(result);
+          } else {
+            res.status(200).json({
+              bookedEntry: {
+                date: date,
+                duration: req.body.duration,
+                activity: req.body.activity,
+                note: req.body.note
+              }
+            });
+          }
+        });
+    } catch (e) {
+      res
+        .status(400)
+        .send('Something went wrong - /book/:date/:duration/:activity/:note');
+      logger.error('/book/:date?/:duration/:activity/:note');
+      logger.info(e.stack);
+    }
+    logger.debug('/book/:date?/:duration/:activity/:note done');
   }
 
   /**
