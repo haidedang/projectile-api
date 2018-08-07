807b38be8e5c4e56567c007cd0dc7c9aa7d80881
Hai Dang <haiduc.dang91@gmail.com>
2018-08-07 12:13:22 +0200

    add reducers

--- src/client/reducers.js
+++ src/client/reducers.js
@@ -1,6 +1,26 @@
-const initialState = {
-  token: false
-};
-const rootReducer = (state = initialState, action) => state;
+import {LOGIN} from './actions'
+import {LOGOUT} from './actions'
 
+const initialState = {
+    token: false
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN:
+            return { 
+                ...state,
+                token: action.token
+            }
+        case LOGOUT:
+            return { 
+                ...state,
+                token:false
+            }
+        default:
+            return state
+    }
+}
+
+export const getToken = state => state.token
 export default rootReducer;
