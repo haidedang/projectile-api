3f9bf53cbb69ed03c9140ac0b5ea001a29499e20
Hai Dang <haiduc.dang91@gmail.com>
2018-08-07 12:13:35 +0200

    add actions for login and logout

---
+++ src/client/actions.js
@@ -0,0 +1,23 @@
+export const LOGIN = 'LOGIN'
+export const LOGOUT = 'LOGOUT'
+
+export function login(token) { 
+  sessionStorage.setItem('token', token)
+  console.log('dispatch successfull')
+    return dispatch => { 
+       dispatch({
+        type: LOGIN,
+        token:sessionStorage.token
+       })
+    }
+}
+
+export function logout(){
+    return dispatch => { 
+        dispatch({ 
+            type:LOGOUT
+        })
+        sessionStorage.removeItem('token');
+        console.log('LOGOUT successfully')
+    }
+}
