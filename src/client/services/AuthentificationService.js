f9403e3794e9276ecf59bdcf99429f6b6e63626a
Hai Dang <haiduc.dang91@gmail.com>
2018-08-08 10:16:50 +0200

    add js Doc Comment

--- src/client/services/AuthentificationService.js
+++ src/client/services/AuthentificationService.js
@@ -1,17 +1,20 @@
-
-
-class AuthentificationService { 
-
-  static async login(username, password){ 
+/**
+ * providing a Service for Client Login through Fetch API
+ */
+class AuthentificationService {
+  static async login(username, password) {
     fetch('http://localhost:3000/api/v1/login', {
       method: 'POST',
-      body: JSON.stringify({username: username, password: password}),
-      headers: { 
-        'Content-Type':'application/json'
+      body: JSON.stringify({ username: username, password: password }),
+      headers: {
+        'Content-Type': 'application/json'
       }
-    }).then(res => {return res.json()})
-    .then(token => console.log(token))
-    .catch(error => console.error('Error:' , error))
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(token => console.log(token))
+      .catch(error => console.error('Error:', error));
   }
 }
 
