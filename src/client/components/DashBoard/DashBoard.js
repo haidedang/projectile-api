2d7434867b4be177cbae213c40b0da7117fdd424
Hai Dang <haiduc.dang91@gmail.com>
2018-08-09 12:51:48 +0200

    add Dashboard

--- src/client/components/DashBoard/DashBoard.js
+++ src/client/components/DashBoard/DashBoard.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import Nav from './Header/Nav'
-import Main from './Main/Main'
+import Nav from './Header/Nav';
+import Main from './Main/Main';
+import { connect } from 'react-redux';
+
+import { fetchSelectOptions } from '../../Actions/PackageActions';
+import ApiCaller from '../../services/ApiCaller';
+import { getToken, getSelectOptions } from '../../reducers';
+import { addSelectOptions } from '../../Actions/PackageActions';
 
 class DashBoard extends React.Component {
+  async componentWillMount() {
+    console.log(sessionStorage.token);
+    console.log(sessionStorage);
+    //Actions required to provide package data for Select Options
+    const api = new ApiCaller(sessionStorage.token);
+    try {
+      let res = await api.callApi('showListProjectile');
+
+      if (res.status == 'error') {
+        console.error(res);
+        return;
+      } else {
+        console.log(res)
+        this.props.dispatch(addSelectOptions(res));
+      }
+    } catch (e) {
+      console.log('Error', e);
+    }
+  }
+
   render() {
     return (
       <div>
         <Nav />
-        <Main />
+        <Main options={this.props.options} />
       </div>
     );
   }
 }
 
-export default DashBoard;
+function mapStateToProps(state) {
+  return {
+    token: getToken(state),
+    options: getSelectOptions(state)
+  };
+}
+
+export default connect(mapStateToProps)(DashBoard);
