4b83a5873cd64d23324ed6ddb6fa73bddd30950c
Michael Bathke <michael.bathke@sevenval.com>
2018-08-09 15:30:42 +0200

    Fixed some linting issues

--- src/client/components/App/App.js
+++ src/client/components/App/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './app.css';
+import React, { Component } from "react";
+import "./app.css";
 // import { Router, browserHistory } from 'react-router';
 // import { Route, IndexRoute } from 'react-router';
-import Login from '../Login/Login';
+import Login from "../Login/Login";
 // import { Link } from 'react-router-dom';
 // import { connect } from 'react-redux';
 // import { getToken } from '../../reducers';
-import DashBoard from '../DashBoard/DashBoard';
+import DashBoard from "../DashBoard/DashBoard";
 
 class App extends Component {
   render() {
-    if (sessionStorage.token == undefined) {
+    if (sessionStorage.token === undefined) {
       return (
         <div className="jumbotron">
           <div id="root" className="container">
